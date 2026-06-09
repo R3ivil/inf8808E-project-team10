69b2910f9daffdba7acd15c0bef033ee4dccbfbf
R3ivil <olivierohara@outlook.com>
2026-06-08 23:37:52 -0400

    feat main

--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
 import * as d3 from 'd3'
 
+const VARIABLES = [
+  'ResponseId', 'MainBranch', 'Age', 'EdLevel', 'Employment',
+  'YearsCode', 'WorkExp', 'DevType', 'OrgSize', 'ICorPM',
+  'RemoteWork', 'Industry', 'AISelect', 'AISent', 'AIAcc',
+  'AIComplex', 'AIThreat', 'AIFrustration', 'AIAgents',
+  'AIAgentChange', 'AIHuman', 'SOFriction'
+]
+
 Promise.all([
   d3.csv('/data/results.csv'),
   d3.csv('/data/schema.csv')
 ]).then(([results, schema]) => {
-  console.log('Respondents:', results.length)
-  console.log('First row:', results[0])
-  console.log('Schema:', schema.slice(0, 5))
+  
+  const data = results.map(row => {
+    const filtered = {}
+    VARIABLES.forEach(v => { filtered[v] = row[v] })
+    return filtered
+  })
+
+  console.log('Respondents:', data.length)
+  console.log('First row:', data[0])
+  console.log('Variables:', Object.keys(data[0]))
 })
